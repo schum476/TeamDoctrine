a1d1d46406ab3ece9f599abdefbb31b95280c497
Geoffrey Schumacher <schum476@serenity.morris.umn.edu>
2013-10-01 21:16:54 -0500

    Got all the tests for disemvowel to pass!`

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,14 +1,30 @@
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 #include "disemvowel.h"
 
 char* disemvowel(char* str) {
-  char* result;
-  result = "";
-  for(int i = 0; i < strlen(str); i++){
-    if(str[i] != "a" || str[i] != "e" || str[i] != "i" || str[i] = "o" || str[i] = "u") {
-      //dostufs
+
+  int vowelCount, i, resultLength, j, length;
+  length = strlen(str);
+    
+  for (i = 0; i < length; i++) {
+    if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U') {
+      vowelCount++;
     }
   }
-  return "";
+
+  char* result;
+  resultLength = length - vowelCount + 1;
+  result = calloc(resultLength, sizeof(char));
+  j = 0;
+  for(i = 0; i < length; i++){
+    if(str[i] != 'a' && str[i] != 'e' && str[i] != 'i' && str[i] != 'o' && str[i] != 'u' && str[i] != 'A' && str[i] != 'E' && str[i] != 'I' && str[i] != 'O' && str[i] != 'U') {
+      result[j] = str[i];
+      j++;
+    }
+  }
+  result[resultLength - 1] = '\0';
+  return result;
 }
